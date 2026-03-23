bc7d656780167944a80152f884c590836df12a1a
wrj80z <major.merritt14@icloud.com>
2026-03-22 21:55:02 -0700

    fixed not loading again

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -35626,7 +35626,7 @@ run(function()
 		end
 	end
 	
-	AutoHonor = vape.Categories.AltFarm:CreateModule({
+	AutoHonor = vape.Categories.Minigames:CreateModule({
 		Name = "AutoHonor",
 		Function = function(callback)
 			if callback then
