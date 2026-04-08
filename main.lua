578789fb1c5882d447021fab66560a9291a07f89
poopparty <obiora.ezenekwe7@gmail.com>
2026-04-08 16:18:18 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,3 @@
---This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.
 repeat task.wait() until game:IsLoaded()
 if shared.vape then shared.vape:Uninject() end
 
